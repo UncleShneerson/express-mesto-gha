0f0dffed18267e96d8848d6fd3e48011947c755e
Eugene Krasnoshlykov <UncleShneerson@yandex.ru>
2023-08-04 00:45:49 -0300

    fix validation test

--- routes/wrongPath.js
+++ routes/wrongPath.js
@@ -4,6 +4,6 @@ const sendAllert = (req, res) => {
   res.status(404).send({ message: 'Путь не найден' });
 };
 
-router.get('/', sendAllert);
+router.patch('/', sendAllert);
 
 module.exports = router;
